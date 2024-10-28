2d5f67b76943286b544ba6db00b8bb0101ead454
karan <jangdekaran54@gmail.com>
2024-10-28 16:47:21 +0530

    few update

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+const PORT = 4000;
 //Require Routes
 
 const userRoutes = require("./routes/UserRoutes");
 const googleRoutes = require("./routes/GoogleRoutes");
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+
 
 app.use(userRoutes);
 app.use(googleRoutes);
 
 //DATABASE CONNECTION
-module.exports = app;
+const db = mongoose.connect(process.env.DB_URL, {});
+
+db.then(() => {
+  console.log("HELL Yeah");
+}).catch((err) => {
+  console.log("Failed to connect to DB", err);
+});
+
+//server creation
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
