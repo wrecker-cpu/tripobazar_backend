a81689492f61c3ca6d4d3079946d4130059e1e01
karan <jangdekaran54@gmail.com>
2024-10-29 13:04:07 +0530

    database

--- app.js
+++ app.js
@@ -23,20 +23,26 @@ app.use("/api/users", userRoutes);
 app.use("/api/google", googleRoutes);
 
 // DATABASE CONNECTION
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error("Failed to connect to DB", err);
+const connectDB = async (retries = 5) => {
+  while (retries) {
+    try {
+      await mongoose.connect(process.env.DB_URL);
+      console.log("Connected to MongoDB");
+      break; // Exit the loop on successful connection
+    } catch (err) {
+      console.error("Failed to connect to DB", err);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      await new Promise((res) => setTimeout(res, 5000)); // Wait 5 seconds before retrying
+    }
+  }
+
+  if (retries === 0) {
+    console.error("Could not connect to MongoDB after multiple attempts.");
     process.exit(1); // Exit process if connection fails
   }
 };
 
-// Call the function to initialize the connection
 connectDB();
 
 // Server creation
