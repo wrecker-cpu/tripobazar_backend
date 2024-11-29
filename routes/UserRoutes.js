45103cb85be9c61f5b62b4325672c759d86e1078
karan <jangdekaran54@gmail.com>
2024-11-29 15:42:42 +0530

    userRoutes

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -9,9 +9,9 @@ router.post("/", userController.createUser);
 router.get("/", auth.protect, auth.restrictToAdmin, userController.getAllUser);
 
 // Route for getting, updating, and deleting a user by ID
-router.get("/data/:id", auth.protect, userController.getUserbyID);
-router.put("/updateAll", userController.updateAllUsers); // Ensure this is above the dynamic route
+router.get("/data/:id",auth.protect, userController.getUserbyID);
 router.put("/:id", userController.updateUser);
+router.put("/updateAll", userController.updateAllUsers);
 router.delete("/:id", userController.deleteUser);
 
 // User login route
